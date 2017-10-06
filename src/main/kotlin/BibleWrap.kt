4b87f092b9e8aada8f440735c81a496987528ad3
Ryan M. Hoffman <rmh.software@gmail.com>
2017-10-05 20:51:47 -0400

    update documentation and add getChapter method shell

--- src/main/kotlin/BibleWrap.kt
+++ src/main/kotlin/BibleWrap.kt
@@ -13,6 +13,11 @@ class BibleWrap {
 	 */
 	private val baseURL = "http://getbible.net/json?p="
 
+	/**
+	 * Blanket function to get any passage with a valid endpoint. This should be used
+	 * with caution because it can easily be the cause of an error if you attempt to
+	 * call a non-existent endpoint.
+	 */
 	fun getPassage(passage: String): Boolean {
 		// Connect to the URL.
 		val connection = URL(baseURL + passage).openConnection() as HttpURLConnection
@@ -22,6 +27,14 @@ class BibleWrap {
 		val data = connection.inputStream.bufferedReader().readText()
 
 		println(data)
+		return true
+	}
+
+	/**
+	 * Function to get any single chapter from the specified book.
+	 */
+	fun getChapter(book: String, chapter, Int): Boolean {
+		// TODO: get the chapter.
 		return true
 	}
 }
